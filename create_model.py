4bb555fe9b319a231bbc84870d3697b45d966933
MingshiYangUIUC <mingshi3@illinois.edu>
2026-04-28 21:25:15 -0500

    use a point model that predict territory instead of wind rate, use a policy model to 'better' select possible actions and 'speed up' selfplay training

--- create_model.py
+++ create_model.py
@@ -8,7 +8,7 @@ import datetime
 wd = os.path.dirname(__file__)
 boardsize = 8
 
-m, X, B, c = 4, boardsize, 16, 64  # m input channels, X*X input size, N residual blocks, c channels
+m, X, B, c = 4, boardsize, 24, 64  # m input channels, X*X input size, N residual blocks, c channels
 mlp_hidden_sizes = [256]  # Sizes of hidden layers in the MLP
 Qmodel = P_V0_1(m, X, B, c, mlp_hidden_sizes)
 
@@ -18,7 +18,7 @@ Qmodel.eval()
 #print(Qmodel.get_model_description())
 pi = Qmodel.get_model_description()
 pi['time_created'] = datetime.datetime.now(datetime.UTC).strftime('UTC %Y-%m-%dT%H:%M:%S')
-pi['train_episodes'] = 1200000
+pi['train_episodes'] = 4735000
 print(pi)
 torch.save({'state_dict': Qmodel.state_dict(), 'parameter_info': pi}, os.path.join(wd,'data','Amazon_model_Points.pth'))
 
